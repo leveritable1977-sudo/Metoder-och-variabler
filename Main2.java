f2a58d1873d36d3c32ca87606a6d8cc30a8245ca
leveritable1977-sudo <leveritable1977@gmail.com>
2026-09-14 18:37:48 +0200

    skapar metoden med parameter

---
+++ Main2.java
@@ -0,0 +1,12 @@
+public class Main2 {
+    // Steg 2: Metoden tar emot en parameter 'namn' av typen String
+    public static void halsa(String namn) {
+        System.out.println("Hej på dig " + namn + "!");
+    }
+
+    public static void main(String[] args) {
+        // Steg 4: Vi anropar metoden och skickar med ett namn i parentesen
+        halsa("Anna");
+        halsa("Mohammed");
+         }
+}
