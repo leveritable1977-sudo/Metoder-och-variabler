687bbfd9f32a504afa58d979dd392e5d0d350234
leveritable1977-sudo <leveritable1977@gmail.com>
2026-09-14 18:18:53 +0200

    skapar enkel anropade metoden

---
+++ Main.java
@@ -0,0 +1,13 @@
+public class Main { // Steg 1
+
+    // Steg 2: Metoden definieras
+    public static void halsa() { 
+        // Steg 3: Instruktionen (statement) slutar med ;
+        System.out.println("Hej på dig!"); 
+    } 
+
+    // Steg 4: Main-metoden (starten)
+    public static void main(String[] args) { 
+        halsa(); // Steg 5: Anropet
+    }
+}
